35b31f37a81cba03da7852670426627eeb8d32ca
BrenoNatal <brenonatal3007@gmail.com>
2024-12-20 15:28:37 -0300

    add: account style, entrar com account_num

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,23 @@
 class Account < ApplicationRecord
+  attr_accessor :login
+
+  before_validation :set_defaults
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
+
+
+
+  validates :account_num, uniqueness: true, length: { is: 5 }
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where([ "account_num = :value",
+      { value: login.strip. downcase } ]).first
+  end
+
+  def set_defaults
+    self.balance = 0 if self.balance.blank?
+  end
 end
